9443d0458e15767aaeac772f8ade0bdb0b308a72
Abisek Mishra <abisek@pop-os.localdomain>
2019-06-19 09:34:54 +0530

    Fetching papers from arxiv api

---
+++ fetch.py
@@ -0,0 +1,68 @@
+import requests
+import feedparser
+import time
+import threading
+import pickle
+# from tqdm.auto import tqdm
+import sys
+
+
+class Paper:
+
+    def __init__(self, _id, title, version, summary, authors, link):
+        self._id = _id
+        self.title = title
+        self.version = version
+        self.summary = summary
+        self.authors = authors
+        self.link = link
+        self.replaceVersion = True
+
+    # Method to notify that if a new version comes remove the old version from the database. Default is true.
+    # def replaceOldVersion(self, flag):
+    #     self.replaceVersion = flag
+
+
+class GetPapers:
+
+    def __init__(self, papers_per_call=100, sleep_time=5, max_papers=10000):
+        self.base_url = 'http://export.arxiv.org/api/query?'
+        self.search_query = 'cat:cs.CV+OR+cat:cs.AI+OR+cat:cs.LG+OR+cat:cs.CL+OR+cat:cs.NE+OR+cat:stat.ML'
+        self.papers_per_call = papers_per_call
+        self.start_index = 0
+        self.max_papers = max_papers
+        self.sleep_time = sleep_time
+        self.paper_ids = []
+
+    def start(self):
+
+        sys.stdout.write("Starting fetching papers.....\n")
+        for index in range(self.start_index, self.max_papers, self.papers_per_call):
+
+            query_string = f"search_query={self.search_query}&start={index}&sortBy=lastUpdatedDate&" \
+                                                                                f"max_results={self.papers_per_call} "
+
+            final_url = self.base_url + query_string
+
+            res = requests.get(final_url)
+            if res.status_code == 200:
+                sys.stdout.write(f"\r{index + 100} papers are parsed")
+                id_versions,papers = self.parseResponse(res)
+                self._pickle_papers(papers)
+                self.paper_ids.append(id_versions)
+
+            else:
+                res = self._retry(index,final_url)
+                if not res:
+                    continue
+                else:
+                    pass
+
+    def parseResponse(self, response):
+        pass
+
+    def _pickle_papers(self):
+        pass
+
+    def _parse_id_version(self, paper_url):
+        pass
