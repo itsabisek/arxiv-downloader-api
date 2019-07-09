8630eedf90fa97d90dd4c38a103ef09bb7a4ed19
itsabisek <avisekmishra81@ymail.com>
2019-07-09 09:14:55 +0530

    Added new driver script

--- driver_new.py
+++ driver_new.py
@@ -1,20 +1,37 @@
+import os
 import pickle
-from fetch import ArxivDl
 import time
-import traceback
-import sys, select
 
-papers = []
+from fetch import ArxivDl
 
-categories = (
-    'cs.AI', 'cs.AR', 'cs.CC', 'cs.CG', 'cs.CL', 'cs.CR', 'cs.CV', 'cs.DB', 'cs.DC', 'cs.DL', 'cs.DM', 'cs.DS', 'cs.FL',
-    'cs.GR', 'cs.GT', 'cs.HC', 'cs.IR', 'cs.IT', 'cs.LG', 'cs.LO', 'cs.MA', 'cs.MM', 'cs.MS', 'cs.NA', 'cs.NE', 'cs.NI',
-    'cs.OS', 'cs.PL', 'cs.RO', 'cs.SC', 'cs.SE', 'cs.SY', 'q-bio.BM', 'q-bio.CB', 'q-bio.GN', 'q-bio.MN', 'q-bio.NC',
-    'q-bio.PE', 'q-bio.SC', 'q-bio.TO', 'stat.AP', 'stat.CO', 'stat.ME', 'stat.ML', 'stat.TH', 'eess.AS', 'eess.IV',
-    'eess.SP')
+with open('categories', 'rb') as file:
+    categories = pickle.load(file)
 
+for category in categories.keys():
 
+    papers_to_save = []
+    start_index = 0
+    counter = 0
 
+    print(f"\nFetching papers from {categories[category]}......")
 
+    while True:
+        parser = ArxivDl(start_index=start_index, search_query=category)
+        start_index, papers = parser.start()
+        papers_to_save.extend(papers)
 
+        if len(papers) == 0:
+            counter += 1
+        elif len(papers) != 0 and counter > 0:
+            counter = 0
 
+        if counter != 10:
+            print(f"\nCurrently {len(papers)} papers fetched. Will continue after 10 minutes")
+            time.sleep(600)
+            continue
+        break
+
+    print(f"\nPickle-ing {len(papers_to_save)} papers")
+    file_name = os.path.join(os.path.abspath(os.curdir),'dumps',categories[category]+'.papers')
+    with open(file_name,'wb+') as file:
+        pickle.dump(papers_to_save,file_name)
